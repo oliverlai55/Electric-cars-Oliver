eee95d5347b1ea77d7c33af584c5d4bdd2bfcc8b
Wei lun Lai <oliverlai55@gmail.com>
2015-12-11 14:48:43 -0500

    add changes to api photo.js schema

--- routes/api.js
+++ routes/api.js
@@ -30,6 +30,7 @@ router.get('/photos/get', function (req, res, next) {
 
 router.post('/photos/post', function (req,res, next){
 	var photo = new Photo();
+	photo.name = req.body.name;
 	photo.image = req.body.image;
 	photo.totalVotes = 0;
 
@@ -44,12 +45,19 @@ router.post('/photos/post', function (req,res, next){
 
 
 router.put('/photos/update', function (req, res, next){
-	Photo.findById(req.params.photo_id, function (err, photosResult){
+	Photo.find({name: req.params.name}, function (err, photosResult){
+		//same as mongo: db.collection('photos').find({image: req.body.photo}).toArray
+		console.log(photosResult);
+		console.log("================");
+	console.log(req.params.name);
+		console.log("================");
+		console.log(req.params.photo);
 	 if(err){
 	 	console.log(err);
 	 }else{
 	 	photosResult.image = req.params.photo; //change the property of the object
 	 	//we got from Mongo
+	 	//set the results image to whatever is in params.photo
 	 	photoResult.save(function(err){
 	 		if(err){
 	 			console.log(err);
--- routes/index.js
+++ routes/index.js
@@ -94,7 +94,7 @@ router.get('/standings',function(req, res, next){
 	
 });
 
-router.post('*', function (req,res,next){
+router.post('/electric', function (req,res,next){
 	if(req.url == '/electric'){
 		var page = 'electric';
 	}else if (req.url == '/poser'){
